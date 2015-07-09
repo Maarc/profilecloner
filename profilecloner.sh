f96555ea2ca8c4fca6d69bf7e243a2061a89f799
Maarc <marc@zottner.com>
2015-07-09 16:40:23 +0200

    profilecloner.sh points to target directory

--- profilecloner.sh
+++ profilecloner.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-CLONER=$(dirname "$0")/profilecloner.jar
+CLONER=$(dirname "$0")/target/profilecloner-2015-06-25.jar
 
-java -cp $JBOSS_HOME/bin/client/jboss-cli-client.jar:$CLONER org.jboss.tfonteyne.profilecloner.Main "$@"
+COMMAND="java -cp $JBOSS_HOME/bin/client/jboss-cli-client.jar:$CLONER org.jboss.tfonteyne.profilecloner.Main $@"
+echo "# ${COMMAND}"
+eval "${COMMAND}"
 
